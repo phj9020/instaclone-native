8d50ca0731b413acf093f904e8971a5ae91ad4cf
HanJin Park <phj9020@hotmail.com>
2021-06-10 13:06:42 +0900

    AppLoading added in order to preload Ionicons from expo/vector-icons and Assets from local asset folder

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Ionicons} from '@expo/vector-icons'; 
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+
+  const preload = () => {
+    // font icons to load
+    const fontsToLoad = [Ionicons.font];
+    const fontPromise = fontsToLoad.map(font => Font.loadAsync(font));
+
+    // image assets to load
+    const imagesToLoad = [require("./assets/logo.png"), "https://upload.wikimedia.org/wikipedia/commons/2/2a/Instagram_logo.svg"];
+    const imagePromise = imagesToLoad.map(image => Asset.loadAsync(image));
+
+    return Promise.all([...fontPromise, ...imagePromise]);
+  };
+
+  if(loading) {
+    return <AppLoading startAsync={preload} onError={console.warn} onFinish={onFinish} />
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
