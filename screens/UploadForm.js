06625d21989f6d79443ad823ec5a02681f889fd6
HanJin Park <phj9020@hotmail.com>
2021-06-24 19:59:36 +0900

    UploadForm form styling added & uploadPhoto mutation is stll working on

--- screens/UploadForm.js
+++ screens/UploadForm.js
@@ -1,34 +1,98 @@
 import React, {useEffect} from 'react';
 import styled from 'styled-components/native';
-import {View, Text, TouchableOpacity} from 'react-native';
+import { ActivityIndicator, TouchableOpacity} from 'react-native';
+import DismissKeyboard from '../components/DismissKeyboard';
+import { useForm } from 'react-hook-form';
+import { gql, useMutation } from "@apollo/client";
+import { FEED_PHOTO } from '../fragments';
 
+const Container = styled.View`
+    flex:1;
+    background-color: black;
+    padding: 0px 20px;
+`
 
+const Photo = styled.Image`
+    height: 350px;
+`
+
+const CaptionContainer = styled.View`
+    margin-top: 30px;
+`
+
+const Caption = styled.TextInput`
+    background-color: ${props => props.theme.boxColor.backgroundColor};
+    color: black;
+    padding: 5px 10px;
+    border-radius: 7px;
+`
 const HeaderRightText = styled.Text`
     color: ${props => props.theme.accent.backgroundColor};
     font-weight: bold;
     margin-right: 5px;
 `
 
+const UPLOAD_PHOTO_MUTATION = gql`
+    mutation uploadPhoto($file: Upload!, $caption: String) {
+        uploadPhoto(file: $file, caption: $caption) {
+            ...FeedPhoto
+        }
+    }
+    ${FEED_PHOTO}
+`
+
 function UploadForm({navigation, route}) {
+    const {register, handleSubmit, setValue} = useForm();
     const {file} = route?.params;
-    console.log(route);
+
+
+    const [uploadPhoto, {loading}] = useMutation(UPLOAD_PHOTO_MUTATION)
 
     const HeaderRight = () => (
-        <TouchableOpacity>
-            <HeaderRightText>Upload</HeaderRightText>
+        <TouchableOpacity onPress={handleSubmit(onValid)}>
+            <HeaderRightText>Done</HeaderRightText>
         </TouchableOpacity>
-    )
+    );
+
+    const HeaderRightLoading = () => <ActivityIndicator style={{marginRight: 5}} size="small" color="#0095f6" />
 
     useEffect(() => {
-        navigation.setOptions({
-            headerRight: HeaderRight
-        });
+        register("caption")
     },[])
+    
+    useEffect(() => {
+        navigation.setOptions({
+            headerRight: loading ?  HeaderRightLoading : HeaderRight,
+            ...(loading && { headerLeft: ()=> null }),
+        });
+    },[loading])
+
+    const onValid = (data) => {
+        // console.log(data);
+        uploadPhoto({
+            variables: {
+                // to do file upload variables
+                file: {},
+                caption: data?.caption
+            }
+        })
+    };
 
     return (
-        <View>
-            <Text>Upload</Text>
-        </View>
+        <DismissKeyboard>
+            <Container>
+                <Photo resizeMode="contain" source={{uri: file}} />
+                <CaptionContainer>
+                    <Caption 
+                        returnKeyType="done"
+                        placeholder="Write a caption"
+                        placeholderTextColor="rgba(0,0,0, 0.5)"
+                        onChangeText={(text) => setValue("caption", text)}
+                        onSubmitEditing={handleSubmit(onValid)}
+                    />
+                </CaptionContainer>
+            </Container>
+        </DismissKeyboard>
     )
 }
 
