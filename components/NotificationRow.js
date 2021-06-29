9a10950029fa4acefcdc25742a1a3eac48f09b58
HanJin Park <phj9020@hotmail.com>
2021-06-29 23:10:08 +0900

    deleteNotification feature added (*deleteNotification Mutation)

--- components/NotificationRow.js
+++ components/NotificationRow.js
@@ -3,6 +3,16 @@ import styled from 'styled-components/native';
 import {View} from 'react-native';
 import moment from 'moment';
 import { Ionicons } from '@expo/vector-icons'; 
+import {gql, useMutation} from '@apollo/client';
+
+const DELETE_NOTIFICATION = gql`
+    mutation deleteNotification($id: Int!) {
+        deleteNotification(id: $id) {
+            ok
+            error
+        }
+    }
+`
 
 const Container = styled.View`
     background-color: ${props => props.theme.boxColor.backgroundColor};
@@ -25,13 +35,48 @@ const IoniconsStyle = styled(Ionicons)`
     color: red;
 `
 
-function NotificationRow({payload, createdAt}) {
+function NotificationRow({id, payload, createdAt}) {
+
+    const deleteNotificationUpdate = (cache, result) => {
+        const {data : {deleteNotification : {ok}}} = result; 
+
+        if(ok) {
+            const fragmentId = `Notification:${id}`;
+
+            cache.evict({
+                id: fragmentId
+            });
+
+            cache.modify({
+                id: fragmentId,
+                fields: {
+                    seeNotification(prev) {
+                        return [...prev]; 
+                    }
+                }
+            })
+        }
+
+    }
+
+    const [deleteNotification] = useMutation(DELETE_NOTIFICATION, {
+        update: deleteNotificationUpdate,
+    });
+
+    const deleteNoti = (selectedId)=> {
+        deleteNotification({
+            variables: {
+                id: selectedId
+            }
+        });
+    };
+    
     return (
-        <Container>
+        <Container id={id}>
             <Text>{payload}</Text>
             <DividerView>
                 <Text>{moment.unix(createdAt / 1000).endOf('day').fromNow()}</Text>
-                <DeleteButton>
+                <DeleteButton onPress={()=> deleteNoti(id)}>
                     <IoniconsStyle name="remove-circle-outline" size={24}  />
                 </DeleteButton>
             </DividerView>
